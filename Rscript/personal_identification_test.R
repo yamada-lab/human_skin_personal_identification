9f735ced11007b0e7a70c0c23a0e8c19f30dc0ac
HikaruWatanabe <33887272+HikaruWatanabe@users.noreply.github.com>
2017-11-24 19:51:13 +0900

    Add files via upload

---
+++ Rscript/personal_identification_test.R
@@ -0,0 +1,100 @@
+#################################################################
+## created by Hikaru Watanabe at Tokyo Institute of Technology ##
+## 2017-11-24                                                  ##
+## email<watanabe.h.ap@m.titech.ac.jp>                         ##
+#################################################################
+
+# setting directory
+setwd("/Users/kevalkyrie777/Desktop/Projects/skin_microbiome/GitHub/")
+
+# function of leave one out method (removing same sample with query from reference samples)
+personal_identification <- function(d.dist.m.query_reference, sample_individual){
+  query     <- rownames(d.dist.m.query_reference)
+  reference <- colnames(d.dist.m.query_reference)
+
+  rownames(sample_individual) <- as.vector(sample_individual$sample)
+  df_query     <- sample_individual[query    ,]
+  df_reference <- sample_individual[reference,]
+  
+  # calcurating the score of individual classification
+  # list up the learning_data
+  individual <- as.vector(unique(sample_individual$individual))
+  
+  mat           <- matrix(0,
+                          nrow=length(individual),
+                          ncol=length(individual))
+  rownames(mat) <- individual
+  colnames(mat) <- individual
+  
+  results        <- list()
+  results$matrix <- mat
+  results$score  <- 0
+  score          <- 0
+  
+  for(q in query){
+    
+    df_reference_temp   <- subset(df_reference, sample!=q)
+    using_reference     <- as.vector(df_reference_temp$sample)
+    individual_meandist <- data.frame(individual=NA, distance=NA)
+    
+    for(ind in individual){
+      one_individual           <- subset(df_reference_temp, individual==ind)
+      extract_sample           <- as.vector(one_individual$sample)
+      distance                 <- mean(d.dist.m.query_reference[q, extract_sample])
+      individual_meandist_temp <- data.frame(individual=ind, distance=distance)
+      individual_meandist      <- rbind(individual_meandist, individual_meandist_temp)
+    }
+    
+    individual_meandist2 <- na.omit(individual_meandist)
+    
+    query_individual     <- as.vector(sample_individual[sample_individual$sample == q,2])
+    nearest_individual   <- individual_meandist2[which.min(individual_meandist2$distance),1]
+    mat[query_individual, nearest_individual] <- mat[query_individual, nearest_individual] + 1
+    
+    if(query_individual== nearest_individual){
+      score = score + 1
+    }
+    
+  }
+    
+  results$matrix <- mat
+  results$score  <- score/(length(query))
+  
+  return(results)
+}
+
+# performing to test in our data
+data   <- read.table("matrix/test_data.otu",
+                     sep="\t",header=T,row.names=1)
+sample_individual <- read.table("matrix/test_data.list",
+                                sep="\t",header=T)
+d.rate <- prop.table(as.matrix(data),2)
+d.dist                   <- dist(t(d.rate), method="canberra")
+d.dist.m.query_reference <- as.matrix(d.dist)
+personal_identification(d.dist.m.query_reference, sample_individual)
+
+# personal identification using our data
+data   <- read.table("matrix/skin_16S_ourdata.otutable",
+                    sep="\t",header=T,row.names=1)
+d.rate <- prop.table(as.matrix(data),2)
+
+# making list of sample and individual
+sample_individual <- read.table("matrix/skin_16S_ourdata.list",
+                                sep="\t",header=T)
+
+# calcurating to distance between all samples
+d.dist                   <- dist(t(d.rate), method="canberra")
+d.dist.m.query_reference <- as.matrix(d.dist)
+
+# Making sample and inddividaul table
+personal_identification(d.dist.m.query_reference, sample_individual)
+
+# Query is 1st year's samples, reference is 2nd year's samples.
+data.1st.2nd <- d.dist.m.query_reference[c(1:3,  7:9 , 13:15, 19:21, 25:27, 31:33, 37:39, 43:45, 49:51, 55:57, 61:63),
+                                         c(4:6, 10:12, 16:18, 22:24, 28:30, 34:36, 40:42, 46:48, 52:54, 58:60, 64:66)]
+personal_identification(data.1st.2nd, sample_individual)
+
+# Query is 2nd year's samples, reference is 1st year's samples.
+data.2nd.1st <- d.dist.m.query_reference[c(4:6, 10:12, 16:18, 22:24, 28:30, 34:36, 40:42, 46:48, 52:54, 58:60, 64:66),
+                                         c(1:3,  7:9 , 13:15, 19:21, 25:27, 31:33, 37:39, 43:45, 49:51, 55:57, 61:63)]
+personal_identification(data.2nd.1st, sample_individual)
